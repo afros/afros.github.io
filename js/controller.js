0d264602f89b00ea71ec5ed8c381494e11cd482f
Delly Christian Fofie <fofiedell@gmail.com>
2016-04-13 00:44:38 +0200

    angular translate and first translations

--- js/controller.js
+++ js/controller.js
@@ -1,12 +1,10 @@
 
 angular.module('podium').controller('controller', function($scope, $translate) {
 	$scope.translate = function(lang) {
-		console.log("test");
 		$translate.use(lang);
 	}
 
 	$scope.getLanguage = function() {
-		console.log("test");
 		return $translate.use();
 	}
 });
--- js/script.js
+++ js/script.js
@@ -2,22 +2,34 @@ angular.module('podium', ['pascalprecht.translate']);
 
 angular.module('podium').config(function($translateProvider) {
 	$translateProvider.translations('en', {
-    TITLE: 'Hello',
-    FOO: 'This is a paragraph.',
-    BUTTON_LANG_EN: 'english',
-    BUTTON_LANG_DE: 'german'
+    TITLE: 'Discussion Forum',
+    NAVBAR: {
+    	ABOUT: 'Über uns',
+    	WHO: 'Speakers',
+    	CONTACT: 'book a place'
+    },
+    SLOGAN: 'Heilbronn: an useful diaspora',
+    SLOGAN_SUB: 'Organized on behalf of the cultural week of African Network Of heilbronn'
   });
   $translateProvider.translations('de', {
-    TITLE: 'Hallo',
-    FOO: 'Dies ist ein Paragraph.',
-    BUTTON_LANG_EN: 'englisch',
-    BUTTON_LANG_DE: 'deutsch'
+    TITLE: 'Podiumdiskussion',
+    NAVBAR: {
+    	ABOUT: 'Über uns',
+    	WHO: 'Speakers',
+    	CONTACT: 'Reservieren'
+    },
+    SLOGAN: 'Heilbronn: eine nützliche Diaspora',
+    SLOGAN_SUB: 'Organisiert im Auftrag der Kulturwoche von African Network of Heilbronn'
   });
   $translateProvider.translations('fr', {
-    TITLE: 'Hallo',
-    FOO: 'Dies ist ein Paragraph.',
-    BUTTON_LANG_EN: 'englisch',
-    BUTTON_LANG_DE: 'deutsch'
+    TITLE: 'Forum de discussion',
+    NAVBAR: {
+    	ABOUT: 'A propos',
+    	WHO: 'Speakers',
+    	CONTACT: 'Reservez'
+    },
+    SLOGAN: 'Heilbronn: Une diaspora utile',
+    SLOGAN_SUB: 'Organisé pour le compte de la semaine culturelle de l\'African Network Of heilbronn'
   });
-  $translateProvider.preferredLanguage('en');
+  $translateProvider.preferredLanguage('de');
 });
